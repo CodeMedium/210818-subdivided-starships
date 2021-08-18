1eacb70f5f015cbfcfcca60b15bfe2ab58f9f414
SemiGenerative <SemiGenerative@gmail.com>
2021-08-18 14:51:46 -0700

    #1 Starts the Spaceship class,  adds 30-100 ships

--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,39 @@
-function setup() {
+const spaceships = []
+
+/**
+ * Sketch entry point
+ */
+function setup () {
 	createCanvas(windowWidth, windowHeight)
 	background(0, 25, 60)
+
+  for (let i = 0; i < random(30, 100); i++) {
+    new Spaceship()
+  }
 }
 
-function draw() {
-	ellipse(mouseX, mouseY, 20, 20)
+/**
+ * "Game loop"
+ */
+function draw () {
+}
+
+/**
+ * Represents a Spaceship
+ */
+class Spaceship {
+  constructor () {
+    this.size = random(30, 200)
+    this.x = random(0, windowWidth)
+    this.y = random(0, windowHeight)
+    this.rotation = random(0, PI * 2)
+
+    this.draw()
+  }
+
+  draw () {
+    draw()
+    rotate(this.rotation)
+    rect(this.x, this.y, this.size, this.size / 3)
+  }
 }
