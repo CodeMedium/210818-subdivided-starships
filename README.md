0457e862c5bf0ec6cea5bbe7714de6411152b15e
SemiGenerative <SemiGenerative@gmail.com>
2021-08-18 16:57:34 -0700

    #1 Draws subdivisions (only root ship)

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 The goal of this repo is to recreate an animated and interactive version of the cover for "[Code as Creative Medium](https://mitpress.mit.edu/books/code-creative-medium)"
 
 # Controls
-- SPACEBAR - Regenerates the sketch
+- Click - Regenerates the sketch
--- src/sketch.js
+++ src/sketch.js
@@ -11,7 +11,7 @@ function setup () {
 function createShips () {
 	background(0, 25, 60)
   for (let i = 0; i < random(30, 100); i++) {
-    new Spaceship()
+    spaceships.push(new Spaceship())
   }
 }
 
@@ -22,12 +22,13 @@ function draw () {
 }
 
 /**
- * Handle keypress
+ * Recreate scene on mouseclick
  */
 function keyPressed () {
-  if (keyCode === 32) {
-    createShips()
-  }
+  createShips()
+}
+function mouseClicked () {
+  createShips()
 }
 
 /**
@@ -35,16 +36,75 @@ function keyPressed () {
  */
 class Spaceship {
   constructor () {
-    this.size = random(30, 200)
+    this.width = random(30, 200)
+    this.height = this.width / 3
     this.x = random(0, windowWidth)
     this.y = random(0, windowHeight)
     this.rotation = random(0, PI * 2)
+    this.children = this.subdivide()
 
     this.draw()
   }
 
+  /**
+   * Recursive method that subdivides the rectangle
+   */
+   subdivide (depth = 0, parent) {
+    const divisions = [{}, {}, {}, {}]
+
+    if (!parent) {
+      parent = {
+        x: this.x,
+        y: this.y,
+        width: this.width,
+        height: this.height
+      }
+    }
+    
+    // Setup properties and maybe create children
+    divisions.forEach((division, i) => {
+      divisions[i].isDivided = random() > .5
+      divisions[i].fill = [random(255), random(255), random(255)]
+      divisions[i].width = parent.width / 2
+      divisions[i].height = parent.height / 2
+    })
+
+    // NW
+    divisions[0].x = parent.x
+    divisions[0].y = parent.y
+    // NE
+    divisions[1].x = parent.x + parent.width / 2
+    divisions[1].y = parent.y
+    // SE
+    divisions[2].x = parent.x + parent.width / 2
+    divisions[2].y = parent.y + parent.height / 2
+    // SW
+    divisions[3].x = parent.x
+    divisions[3].y = parent.y + parent.height / 2
+
+    // Setup children
+    divisions.forEach((division, i) => {
+      if (divisions[i].isDivided && depth < 4) {
+        divisions[i].children = this.subdivide(++depth)
+      }
+    })
+    
+    return divisions
+  }
+
   draw () {
+    console.clear()
     rotate(this.rotation)
-    rect(this.x, this.y, this.size, this.size / 3)
+    this.drawChildren(this.children)
+  }
+
+  drawChildren (children) {
+    children.forEach(child => {
+      fill(child.fill)
+      rect(child.x, child.y, child.width, child.height)
+      console.log(child.x, child.y, child.width, child.height)
+    })
+
+    children.children && this.drawChildren(children.children)
   }
 }
